4c162a50bd28c1139d49d97f22609699808b162d
jjacob151 <jayne_jacobs@cable.comcast.com>
2020-05-16 18:06:02 -0400

    Updating Analysis Problems Readme.md checkifprime

--- Code/checkifprime/main.go
+++ Code/checkifprime/main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main()  {
 	var inNum int
-	fmt.Println("Enter a positive number")
+	fmt.Println("Enter a whole number:")
 	fmt.Scanln(&inNum)
+	isPrime := checkPrim(inNum)
+	fmt.Println("Is the number Prime: ", isPrime)
+}
+
+func checkPrim(innm int) bool {
+	if innm == 1 || innm == 0 {
+		fmt.Println("The number is not prime")
+		return false
+	}
+	for i := 2; i <= int(math.Floor(math.Sqrt(float64(innm)))); i++ {
+		if innm % i == 0 {
+			fmt.Printf("This number %v is not prime: \n", innm)
+			return false
+		}
+	}
+	return true
 }
